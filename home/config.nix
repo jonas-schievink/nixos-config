b2761e36286242d3ca85dc162dd9e4b68edc731e
Jonas Schievink <jonasschievink@gmail.com>
2019-07-14 16:11:20 +0200

    Clarify nixpkgs configuration

--- home/config.nix
+++ home/config.nix
@@ -1,1 +1,8 @@
-{ allowUnfree = true; }
+# nixpkgs configuration
+#
+# Note that the Nix package manager itself is configured in
+# ~/.config/nix/nix.conf
+{
+  # Allow installation of proprietary packages:
+  allowUnfree = true;
+}
