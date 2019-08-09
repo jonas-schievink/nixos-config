ae0e18d691c2ba644fcd78dc3dc7476f6e3b3531
Jonas Schievink <jonasschievink@gmail.com>
2019-08-09 21:43:25 +0200

    Enable i3 standalone

--- machines/lanthanum/configuration.nix
+++ machines/lanthanum/configuration.nix
@@ -119,6 +119,11 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+  services.xserver.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3-gaps;
+  };
+
   # PostgreSQL database for development
   services.postgresql = {
     enable = true;
