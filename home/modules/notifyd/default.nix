b92107eb64a2d08929de17c90d7a509073bcc655
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2020-07-08 18:17:59 +0200

    xfce4-notifyd: add desktop item

--- home/modules/notifyd/default.nix
+++ home/modules/notifyd/default.nix
@@ -2,8 +2,17 @@
 
 { config, lib, pkgs, ... }:
 
-{
-  home.packages = [ pkgs.xfce.xfce4-notifyd ];
+let
+  notifyd = pkgs.xfce.xfce4-notifyd;
+  # The package has its own desktop item, but it uses `OnlyShowIn=XFCE`, which is wrong, so we
+  # create our own.
+  desktop-item = pkgs.makeDesktopItem {
+    name = "xfce4-notifyd-config-custom";
+    exec = "${notifyd}/bin/xfce4-notifyd-config";
+    desktopName = "Configure Xfce4 Notification Daemon";
+  };
+in {
+  home.packages = [ notifyd desktop-item ];
 
   systemd.user.services.notifyd = {
     Unit = {
